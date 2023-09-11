541b46fca95aa8338acb443f429700eb053332d5
ATRCORE-UA <126199526+ATRCORE-UA@users.noreply.github.com>
2023-09-11 18:55:08 +0300

    Add files via upload

---
+++ download.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Завантаження додатку</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            background-color: #f0f0f0;
+        }
+
+        h1 {
+            color: #333333;
+        }
+
+        button {
+            padding: 10px 20px;
+            font-size: 16px;
+            border: none;
+            background-color: #007bff;
+            color: #ffffff;
+            cursor: pointer;
+            margin: 10px;
+            border-radius: 5px;
+            text-decoration: none;
+        }
+
+        button:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <h1>Download ATR-APPS</h1>
+    <a href="https://www.dropbox.com/scl/fi/23sc1ysmdofufvrzqe9bc/atr-apps_windows.exe?dl=1" download>
+        <button>Download for Windows</button>
+    </a>
+    <br>
+    <a href="https://www.dropbox.com/s/pdlquwwljfo8cbj/app-release.apk?dl=1" download>
+        <button>Download for Android</button>
+    </a>
+</body>
+</html>
